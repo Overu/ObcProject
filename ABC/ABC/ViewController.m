c91f70a7d7e5b0c52630c7ce31b297d5770d84a9
Overu <fengjianok@gmail.com>
2013-03-19 12:39:54 +0800

    abc

---
+++ ABC/ABC/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  ABC
+//
+//  Created by zheng libao on 13-3-19.
+//  Copyright (c) 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
